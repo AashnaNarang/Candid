67b24d9cb71fd2994108e2eb29a3430203fec241
Aashna Narang <46693188+AashnaNarang@users.noreply.github.com>
2020-06-06 20:36:39 -0400

    Can add a single image from your computer

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php  
+ini_set('mysql.connect_timeout', 300);
+ini_set('default_socket_timeout', 300);
+
+ $connect = mysqli_connect("localhost", "root", "HireMePlease", "test");  
+ if(isset($_POST["insert"]))  
+ {  
+      $file = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
+      $query = "INSERT INTO images(address) VALUES ('$file')";  
+      if(mysqli_query($connect, $query))  
+      {  
+           echo '<script>alert("Image Inserted into Database")</script>';  
+      }  
+      header("Location: index.php");
+      
+ }  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Candid.</title>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+      </head>  
+      <body>  
+           <br /><br />  
+           <div class="container">  
+                <h3 align="center">Candid.</h3>  
+                <br />  
+                <form method="post" enctype="multipart/form-data">  
+                     <input type="file" name="image" id="image" />  
+                     <br />  
+                     <input type="submit" name="insert" id="insert" value="Insert" class="btn btn-info" />  
+                </form>  
+                <br />  
+                <br />  
+                <table class="table table-bordered">  
+                     <tr>  
+                          <th>Images</th>  
+                     </tr>  
+                <?php  
+                $query = "SELECT * FROM images ORDER BY image_id DESC";  
+				$result = mysqli_query($connect, $query); 
+				if (!$result) {
+					printf("Error: %s\n", mysqli_error($connect));
+					exit();
+				} 
+                while($row = mysqli_fetch_array($result))  
+                {  
+                     echo '  
+                          <tr>  
+                               <td>  
+                                    <img src="data:image/jpeg;base64,'.base64_encode($row['address'] ).'" height="400" width="400" />  
+                               </td>  
+                          </tr>  
+                     ';  
+                }  
+                ?>  
+                </table>  
+           </div>  
+      </body>  
+ </html>  
+ <script>  
+ $(document).ready(function(){  
+      $('#insert').click(function(){  
+           var image_name = $('#image').val();  
+           if(image_name == '')  
+           {  
+                alert("Please Select an Image");  
+                return false;  
+           }  
+           else  
+           {  
+                var extension = $('#image').val().split('.').pop().toLowerCase();  
+                if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)  
+                {  
+                     alert('Invalid Image File');  
+                     $('#image').val('');  
+                     return false;  
+                }  
+           }  
+      });  
+ });  
+ </script>  
